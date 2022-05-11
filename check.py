cd904156774dd9ae9d1f8c734fc758626a322060
jsthomas <jsthomas1288@gmail.com>
2022-05-11 14:48:36 -0500

    Included parameters for to make sure the user's inputs are correct

--- check.py
+++ check.py
@@ -1,9 +1,15 @@
 def split_check(total, people):
+    if people <=1:
+        raise ValueError("More than 1 person is required to split the check")
     # This is the cost per person
     return round((total/people), 2)
 
-total_amt = float(input("How much is the total cost of the bill?   "))
-total_ppl = int(input("How many people will be splitting the check?   "))
-amount = split_check(total_amt, total_ppl)
-
-print("Each person owes ${}".format(amount))
+try:
+    total_amt = float(input("How much is the total cost of the bill?   "))
+    total_ppl = int(input("How many people will be splitting the check?   "))
+    amount = split_check(total_amt, total_ppl)
+except ValueError as err:
+    print("Oh no! That's not a valid value. Try again!")
+    print("({})".format(err))
+else:
+    print("Each person owes ${}".format(amount))
